177db93c3bf2b30bab682671e5ba27674c89a616
“majdkilou” <majdonak@hotmail.com>
2023-05-20 05:20:43 +0400

    angular-mat-import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,28 @@ import { AppRoutingModule } from './app-routing.module';
 // Angular Material imports for toolbar and sidenav components
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {CommonModule} from '@angular/common';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatTableModule} from '@angular/material/table';
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule, MAT_DATE_FORMATS } from "@angular/material/core";
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatOptionModule } from '@angular/material/core';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 
 
@@ -34,8 +55,30 @@ import { NavbarComponent } from './navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule,
-    MatIconModule
+    MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatProgressSpinnerModule,
+  MatGridListModule,
+  CommonModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatSnackBarModule,
+  MatDialogModule,
+  MatInputModule,
+  MatTabsModule,
+  MatTableModule,
+  MatAutocompleteModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatOptionModule,
+  MatListModule,
+  MatMenuModule,
+  MatTooltipModule,
+  MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
